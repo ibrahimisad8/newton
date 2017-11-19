bc3f6b5fb9025fbe4e0efdaac2b3458c15ea8b8b
Ibrahim Isa <ibrahimd8@yahoo.com>
2017-11-19 11:40:17 +0100

    Update nw-config.php

--- nw-config.php
+++ nw-config.php
@@ -2,8 +2,6 @@
 /**
  * Configuration File
  * 
- * This file is important to all pages changes can be made to constants if  the 
- * need arises
  * @author Ibrahim Isa
  * @version 1.0
  * @package nw-config
